0fc33858e327d42c5359c7d6f0557fa22f2ab925
MUHAMMAD-ROKY <rokyksy@gmail.com>
2025-09-25 16:40:18 +0600

    DataType

---
+++ class7/dataType.js
@@ -0,0 +1,7 @@
+var n = 25.6
+var f = 125
+var nn = Number('45.8')
+console.log(nn)
+console.log(parseInt(nn))
+console.log(Number.MIN_VALUE)
+console.log(Number.MAX_SAFE_INTEGER)
---
+++ class7/variable.js
@@ -0,0 +1,4 @@
+var name ="kamal Hossain"
+var age = 23
+console.log(name +' knows javascript programming language')
+console.log(age)
