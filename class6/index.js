2ccf964e107c228c5cb1dd944f499bf0d3368c01
MUHAMMAD-ROKY <rokyksy@gmail.com>
2025-09-25 16:01:12 +0600

    Data Type

---
+++ class6/index.js
@@ -0,0 +1,10 @@
+console.log("Hello jack How are you?")
+console.log('hi')
+console.log(657)
+console.log(6.57)
+console.log('Enter your number:'+ 657)
+console.log(5+5);
+console.log('5'+5);
+
+
+console.log(typeof('5'+5));
