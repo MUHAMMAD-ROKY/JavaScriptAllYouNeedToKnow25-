d5e9064d05e523fad747bc6ad2801b265b130f99
MUHAMMAD-ROKY <rokyksy@gmail.com>
2025-09-28 13:46:17 +0600

    whileLoop

---
+++ class7/whileloop.js
@@ -0,0 +1,22 @@
+//while loop
+/*
+var i = 0
+while(i<10){
+    console.log('Hm Kamal')
+    i++
+}
+*/
+
+var isRunning = true
+
+while(isRunning){
+    var rand = Math.floor(Math.random()*10+1)
+    if(rand==9){
+        console.log("Yah i got a car")
+        isRunning = false
+    }
+    else{
+        console.log("you have got"+' '+ rand +' '+'please try again')
+    }
+
+}
