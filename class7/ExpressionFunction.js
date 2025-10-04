7cad6a7cffb008958bb6f4de3a9c2e3db974c416
MUHAMMAD-ROKY <rokyksy@gmail.com>
2025-10-04 17:30:39 +0600

    functionExpression

---
+++ class7/ExpressionFunction.js
@@ -0,0 +1,9 @@
+var addition = function(a,b){
+    return a+b
+}
+
+addition(40,60)
+
+setTimeout(function(){
+    console.log("I will buy a car")
+},3000)
---
+++ class7/functionDef.js
@@ -0,0 +1,23 @@
+//function
+//input output processing
+
+var date = new Date()
+date.getFullYear()
+console.log(date)
+
+
+
+function add(){
+    a = 10,
+    b = 20
+    console.log(a+b)
+}
+add()
+
+function cat(){
+    console.log('Mao Mao Mao')
+}
+//cat()
+for(var i=0; i<10; i++){
+    cat()
+}
